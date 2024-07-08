e6b2b2df580db0b79eb211f263bf9e75bed006eb
Shuvo Islam <shuvoislamcmt3@gmail.com>
2024-07-09 03:38:13 +0600

    PHP echo, comment and php in html chapter

--- index.php
+++ index.php
@@ -1,4 +1,23 @@
 <?php
     echo"I love pizzas <br>";
-    echo"Hello shuvo how are you";
+    echo"IT'S Really Good";
+    // This is a comment
+    /* This 
+       Is 
+       Multiline
+       Comment
+    */ 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <br>
+    <button>Click Me!</button>
+</body>
+</html>
